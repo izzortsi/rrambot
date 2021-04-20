0b4a588d5a5dd33df94a03b52b27e15361931026
Igor Strozzi <istrozzi@matematica.ufrj.br>
2021-04-19 21:01:44 -0300

    some tests with unicorn-binance-websocket-api

---
+++ process_streams.py
@@ -0,0 +1,135 @@
+import logging
+import sys
+
+try:
+    from unicorn_fy.unicorn_fy import UnicornFy
+except ImportError:
+    print("Please install `unicorn-fy`! https://pypi.org/project/unicorn-fy/")
+    sys.exit(1)
+
+
+class BinanceWebSocketApiProcessStreams(object):
+    @staticmethod
+    def process_stream_data(
+        received_stream_data_json, exchange="binance.com", stream_buffer_name="False"
+    ):
+        #
+        #  START HERE!
+        #
+        # `received_stream_data_json` contains one record of raw data from the stream
+        # print it and you see the data like its given from Binance, its hard to work with them, because keys of
+        # parameters are changing from stream to stream and they are not self explaining.
+        #
+        # So if you want, you can use the class `UnicornFy`, it converts the json to a dict and prepares the values.
+        # `depth5` for example doesnt include the symbol, but the unicornfied set includes them, because the class
+        # extracts it from the channel name, makes it upper size and adds it to the returned values.. just print both
+        # to see the difference.
+        # Github: https://github.com/oliver-zehentleitner/unicorn-fy
+        # PyPI: https://pypi.org/project/unicorn-fy/
+        if exchange == "binance.com" or exchange == "binance.com-testnet":
+            unicorn_fied_stream_data = UnicornFy.binance_com_websocket(
+                received_stream_data_json
+            )
+        elif (
+            exchange == "binance.com-futures"
+            or exchange == "binance.com-futures-testnet"
+        ):
+            unicorn_fied_stream_data = UnicornFy.binance_com_futures_websocket(
+                received_stream_data_json
+            )
+        elif (
+            exchange == "binance.com-margin" or exchange == "binance.com-margin-testnet"
+        ):
+            unicorn_fied_stream_data = UnicornFy.binance_com_margin_websocket(
+                received_stream_data_json
+            )
+        elif (
+            exchange == "binance.com-isolated_margin"
+            or exchange == "binance.com-isolated_margin-testnet"
+        ):
+            unicorn_fied_stream_data = UnicornFy.binance_com_margin_websocket(
+                received_stream_data_json
+            )
+        elif exchange == "binance.je":
+            unicorn_fied_stream_data = UnicornFy.binance_je_websocket(
+                received_stream_data_json
+            )
+        elif exchange == "binance.us":
+            unicorn_fied_stream_data = UnicornFy.binance_us_websocket(
+                received_stream_data_json
+            )
+        else:
+            logging.error("Not a valid exchange: " + str(exchange))
+
+        # Now you can call different methods for different `channels`, here called `event_types`.
+        # Its up to you if you call the methods in the bottom of this file or to call other classes which do what
+        # ever you want to be done.
+        try:
+            if unicorn_fied_stream_data["event_type"] == "aggTrade":
+                BinanceWebSocketApiProcessStreams.aggtrade(unicorn_fied_stream_data)
+            elif unicorn_fied_stream_data["event_type"] == "trade":
+                BinanceWebSocketApiProcessStreams.trade(unicorn_fied_stream_data)
+            elif unicorn_fied_stream_data["event_type"] == "kline":
+                BinanceWebSocketApiProcessStreams.kline(unicorn_fied_stream_data)
+            elif unicorn_fied_stream_data["event_type"] == "24hrMiniTicker":
+                BinanceWebSocketApiProcessStreams.miniticker(unicorn_fied_stream_data)
+            elif unicorn_fied_stream_data["event_type"] == "24hrTicker":
+                BinanceWebSocketApiProcessStreams.ticker(unicorn_fied_stream_data)
+            elif unicorn_fied_stream_data["event_type"] == "depth":
+                BinanceWebSocketApiProcessStreams.miniticker(unicorn_fied_stream_data)
+            else:
+                BinanceWebSocketApiProcessStreams.anything_else(
+                    unicorn_fied_stream_data
+                )
+        except KeyError:
+            BinanceWebSocketApiProcessStreams.anything_else(unicorn_fied_stream_data)
+        except TypeError:
+            pass
+
+    @staticmethod
+    def aggtrade(stream_data):
+        # print `aggTrade` data
+        print(stream_data)
+
+    @staticmethod
+    def trade(stream_data):
+        # print `trade` data
+        print(stream_data)
+
+    @staticmethod
+    def kline(stream_data):
+        # print `kline` data
+        print(stream_data)
+
+    @staticmethod
+    def miniticker(stream_data):
+        # print `miniTicker` data
+        print(stream_data)
+
+    @staticmethod
+    def ticker(stream_data):
+        # print `ticker` data
+        print(stream_data)
+
+    @staticmethod
+    def depth(stream_data):
+        # print `depth` data
+        print(stream_data)
+
+    @staticmethod
+    def outboundAccountInfo(stream_data):
+        # print `outboundAccountInfo` data from userData stream
+        print(stream_data)
+
+    @staticmethod
+    def executionReport(stream_data):
+        # print `executionReport` data from userData stream
+        print(stream_data)
+
+    @staticmethod
+    def anything_else(stream_data):
+        print(stream_data)
+
+
+if __name__ == "__main__":
+    print("Dont run this script, its for imports only!")
